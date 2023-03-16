01818449cd99818bca584fd1498b790b25dc5391
adeboy11 <princeadeboy4real1@gmail.com>
2023-03-16 02:53:27 -0700

    c-funtion

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ long long int z;
 float f;
 printf("The size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
-printf("The size of a long: %lu byte(s)\n", (unsigned long)sizeof(y));
-printf("The size of a long long: %lu byte(s)\n", (unsigned long)sizeof(z));
+printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
 printf("The size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
