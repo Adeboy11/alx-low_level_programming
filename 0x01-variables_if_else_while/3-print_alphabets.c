1e4c1b47baf6aeea882567016ab34fb6310070c1
adeboy11 <princeadeboy4real1@gmail.com>
2023-03-17 05:44:31 -0700

    print uppercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: program to input all alphats
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= z; x++)
+		putchar(x);
+	for (x = 'A; x <= Z; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: program to input all alphats
+ * Return: 0
+ */
+int main(void)
+{
+        char x;
+
+        for (x = 'a'; x <= z; x++)
+                putchar(x);
+        putchar('\n');
+        return (0);
+}
