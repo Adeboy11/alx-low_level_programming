c192e8026566072d1402742dcce16835da8ce699
adeboy11 <princeadeboy4real1@gmail.com>
2023-03-21 10:31:18 -0700

    another signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,9 +17,8 @@ _putchar(',');
 _putchar(' ');
 return (1);
 }
-else if
+else if (n == 0)
 {
-(n == 0)
 _putchar('0');
 _putchar(',');
 _putchar(' ');
@@ -30,5 +29,5 @@ else
 _putchar('-');
 _putchar(',');
 _putchar('/');
-{
+}
 }
