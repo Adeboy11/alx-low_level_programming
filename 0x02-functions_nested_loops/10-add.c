cbf5a613776fe88939f1471f6808b37e2050a206
adeboy11 <princeadeboy4real1@gmail.com>
2023-03-21 10:07:19 -0700

    print all times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add() - programs
+ * Description - a program to add two values
+ * @a - first variable
+ * @b -second variable
+ * Return: 1
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
