00be3abc51b93cfa77c0f122df972563a2c3dbab
adeboy11 <princeadeboy4real1@gmail.com>
2023-03-27 09:27:34 -0700

    keygen reloaded

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,17 +9,20 @@
  */
 int main(void)
 {
-srand((unsigned int)(time(0)));
-int i;
-char pwd[100];
-for (i = 0; i < 4; i++)
-{
-pwd[i] = rand() % 9;
-char capLetter = 'A' + (rand() % 26);
-pwd[i + 2] = capLetter;
-char letter = 'a' + (rand() % 26);
-pwd[i + 3] = letter;
-printf("%d%c%c", pwd[i], pwd[i + 2], pwd[i + 3]);
-}
-printf("\n");
+	int i;
+	srand((unsigned int)(time(0)));
+	char pwd[100];
+
+	for (i = 0; i < 4; i++)
+	{
+		pwd[i] = rand() % 9;
+		char capLetter;
+		capLetter = 'A' + (rand() % 26);
+		pwd[i + 2] = capLetter;
+		char letter;
+	       	letter = 'a' + (rand() % 26);
+		pwd[i + 3] = letter;
+		printf("%d%c%c", pwd[i], pwd[i + 2], pwd[i + 3]);
+	}
+	printf("\n");
 }
