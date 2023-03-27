80ee1b0b2f34a49e4a49390be2b9f9500c3313dc
adeboy11 <princeadeboy4real1@gmail.com>
2023-03-27 09:31:21 -0700

    keygen works

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 	int i;
-	srand((unsigned int)(time(0)));
 	char pwd[100];
+	char capLetter;
+	char letter;
 
+	srand((unsigned int)(time(0)));
 	for (i = 0; i < 4; i++)
 	{
 		pwd[i] = rand() % 9;
-		char capLetter;
 		capLetter = 'A' + (rand() % 26);
 		pwd[i + 2] = capLetter;
-		char letter;
 	       	letter = 'a' + (rand() % 26);
 		pwd[i + 3] = letter;
 		printf("%d%c%c", pwd[i], pwd[i + 2], pwd[i + 3]);
