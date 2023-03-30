bfebe93d5de9b9fc3ff90adb6c249d4cb281a6ea
adeboy11 <princeadeboy4real1@gmail.com>
2023-03-30 01:46:39 -0700

    array and string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,16 +9,16 @@
 
 char *string_toupper(char *a)
 {
-int i;
-char *ptr;
+	int i;
+	char *ptr;
 
-ptr = a;
-for (i = 0; a[i] != '\0'; i++)
-{
-if (a[i] >= 'a' && a[i] <= 'z')
-{
-a[i] = a[i] - 32;
-}
-}
-return (ptr);
+	ptr = a;
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+	}
+	return (ptr);
 }
